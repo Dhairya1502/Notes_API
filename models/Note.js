011ddc01445ac24b7728d9751ee561a9f11594ee
Dhairya1502 <dhairyapandit322@gmail.com>
2026-06-20 21:29:00 +0530

    Added timestamps

--- models/Note.js
+++ models/Note.js
@@ -1,18 +1,23 @@
 const mongoose = require("mongoose");
 
-const noteSchema = new mongoose.Schema({
+const noteSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     content: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
-    userId: {
-        type: String,
-        required: true
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
     }
-});
+  },
+  {
+    timestamps: true
+  }
+);
 
 module.exports = mongoose.model("Note", noteSchema);
